5f11583e7d1fa3f831984b62418825d690ce7a8c
Josh Farwell <josh.farwell@gmail.com>
2022-03-16 03:33:04 -0700

    Update config.py
    Add API_MAX_LENGTH config variable.
    
    Also force type to int on variables that need to be ints, they'll come in as strings from the environment.

--- hash_house/config.py
+++ hash_house/config.py
@@ -4,9 +4,10 @@ import os
 class Config:
 
     # Default 10MB/11MB
-    UPLOAD_SIZE_LIMIT = os.environ.get("UPLOAD_SIZE_LIMIT", 1024 * 1024 * 10)
-    MAX_CONTENT_LENGTH = os.environ.get("MAX_CONTENT_LENGTH", 1024 * 1024 * 11)
+    UPLOAD_SIZE_LIMIT = int(os.environ.get("UPLOAD_SIZE_LIMIT", 1024 * 1024 * 10))
+    MAX_CONTENT_LENGTH = int(os.environ.get("MAX_CONTENT_LENGTH", 1024 * 1024 * 11))
 
     S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", '')
 
     APIKEY_FILE_PATH = os.environ.get("APIKEY_FILE_PATH", "/root/hashhouse_keys.json")
+    APIKEY_MAX_LENGTH = int(os.environ.get("APIKEY_MAX_LENGTH", 128))
